95310dbfc12f97e31d3e920e024eea8853b7e917
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2021-07-20 22:08:03 +0900

    feat: setup functions to render Nuxt.js SSR site

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,6 @@
-import * as functions from "firebase-functions";
+import * as functions from "firebase-functions"
+import * as Nuxt from "nuxt"
+import * as nuxtConfig from "./nuxt.config"
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +9,17 @@ import * as functions from "firebase-functions";
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const config = {
+  ...nuxtConfig,
+  dev: false,
+  debug: false,
+  buildDir: 'nuxt'
+}
+
+const nuxt = new Nuxt(config)
+
+export const ssr = functions.https.onRequest(async (request, response) => {
+    await nuxt.ready()
+    nuxt.render(request, response)
+});
