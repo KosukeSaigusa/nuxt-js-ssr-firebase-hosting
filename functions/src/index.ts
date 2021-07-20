6d6d198944353c89c269f6ac057d369bcb5e52ea
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2021-07-20 23:46:07 +0900

    feat: setup firebase hosting

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,8 @@
-import * as functions from "firebase-functions"
-import * as Nuxt from "nuxt"
-import * as nuxtConfig from "./nuxt.config"
+import * as functions from "firebase-functions";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const {Nuxt} = require("nuxt");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const express = require("express");
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -10,16 +12,23 @@ import * as nuxtConfig from "./nuxt.config"
 //   response.send("Hello from Firebase!");
 // });
 
-const config = {
-  ...nuxtConfig,
+const app = express();
+const nuxt = new Nuxt({
   dev: false,
   debug: false,
-  buildDir: 'nuxt'
-}
-
-const nuxt = new Nuxt(config)
-
-export const ssr = functions.https.onRequest(async (request, response) => {
-    await nuxt.ready()
-    nuxt.render(request, response)
+  srcDir: "nuxt",
+  build: {
+    publicPath: "/assets/",
+  },
 });
+
+app.use(async (request: any, response: any) => {
+  return await nuxt.render(request, response);
+});
+
+export const ssr = functions.https.onRequest(app);
+
+// export const ssr = functions.https.onRequest(async (request, response) => {
+//     await nuxt.ready()
+//     nuxt.render(request, response)
+// });
