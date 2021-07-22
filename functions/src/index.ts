3a07755daeac098c33f61289987d570e6af1bca5
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2021-07-22 13:39:07 +0900

    feat: successfully deploy Nuxt.js SSR site by Firebase Hosting

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,34 +1,15 @@
 import * as functions from "firebase-functions";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const {Nuxt} = require("nuxt");
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const express = require("express");
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-const app = express();
 const nuxt = new Nuxt({
   dev: false,
   debug: false,
-  srcDir: "nuxt",
-  build: {
-    publicPath: "/assets/",
-  },
+  buildDir: "nuxt",
 });
 
-app.use(async (request: any, response: any) => {
-  return await nuxt.render(request, response);
-});
-
-export const ssr = functions.https.onRequest(app);
-
-// export const ssr = functions.https.onRequest(async (request, response) => {
-//     await nuxt.ready()
-//     nuxt.render(request, response)
-// });
+export const nuxtApp = functions
+    .https.onRequest(async (request, response) => {
+      await nuxt.ready();
+      nuxt.render(request, response);
+    });
--- nuxtapp/nuxt.config.ts
+++ nuxtapp/nuxt.config.ts
@@ -1,5 +1,5 @@
-// export default {
-module.exports = {
+export default {
+// module.exports = {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'nuxtapp',
